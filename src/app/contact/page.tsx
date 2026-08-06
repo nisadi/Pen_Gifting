9b014bd7f65cb08bac58019fc3a687c723958ce4
Nisadi Wijerathna <nisadiwijerathna@gmail.com>
2026-08-06 11:42:54 +0530

    Contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -45,7 +45,7 @@ export default function Contact() {
       );
   };
   return (
-    <main className="bg-[#1a1a1a] text-white min-h-screen">
+    <main className="bg-[#15043a] text-white min-h-screen">
 
       {/* NAVBAR */}
       <Navbar />
